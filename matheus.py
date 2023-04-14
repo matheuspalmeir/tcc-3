d5221c2c734243d635f10aaa66a927ee00b45100
Matheus Palmeira <matheus_160497@hotmail.com>
2023-04-14 11:42:35 -0300

    Changed gitignore and matheus.py

--- matheus.py
+++ matheus.py
@@ -16,8 +16,8 @@ import time
 begin_timer = time.time()   # Começa o a contar o tempo
 
 #TROCAR NOME DE SALVAR, O ARQUIVO QUE VAI LER E TAMBEM O NUMERO DO MES/ANO
-name_save = '22Nov_2015_VHteste'
-file = 'D:/campo/Dataset/Sentinel-1 (zip)/images/22Nov_2015_VH.tif'
+name_save = '21Mar_2016_VVteste'
+file = 'C:/Users/mathe/Documents/TCC/Dataset/Sentinel-1/Sentinel-1/9_March_2/21Mar_2016_VV.tif'
 #Leitura da imagem 
 raster = rgb2gray(imread(file))
 raster = np.array(raster,dtype = np.float32)
@@ -33,7 +33,7 @@ levels = 256
 #Tamanho da janela
 win = 3
 #Leitura do dataframe de shapely para pegar os pixels certos de cada geometry
-df = gpd.read_file('D:/campo/Dataset/Reference/CampoVerde_Oct2015_Jul2016.shp')
+df = gpd.read_file('./Reference/CampoVerde_Oct2015_Jul2016.shp')
 
 #Pegar o mes referente de cada fotografia - só realizar a mudança na linha 46 coo mes correto
 #            0          1           2         3          4           5          6          7         8         9
@@ -42,7 +42,7 @@ classes = np.unique(df[month])
 class_dict = dict(zip(classes, range(len(classes))))
 
 #Passar todos os parametros, só
-Xs,ys = campo.haralick_features(raster, win, d, theta, levels, props,file,df, month[1],class_dict,name_save)
+Xs,ys = campo.haralick_features(raster, win, d, theta, levels, props,file,df, month[5],class_dict,name_save)
 
 end_timer = time.time() # Termina a contagem de tempo do algoritmo
 print(end_timer-begin_timer)
